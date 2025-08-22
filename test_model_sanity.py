09385a995a762a23a4c59b1e6a6a20ced5248e6b
Soumya0927 <soumyag27402@gmail.com>
2025-08-22 10:36:49 +0000

    updated test file

--- test_model_sanity.py
+++ test_model_sanity.py
@@ -1,19 +1,21 @@
+import os
 import pandas as pd
 import pickle
 from sklearn.metrics import roc_auc_score
-from pathlib import Path
+
 
 # Define the data folder path relative to project root
 def test_model_auc():
     # Load a small test set and trained model (mock or actual)
 
 # Get project root folder from the file location
-    PROJECT_ROOT = Path(__file__).resolve().parent.parent  # Adjust number of `.parent` levels as needed
+  
 
 # Define the data folder path relative to project root
-    DATA_DIR = PROJECT_ROOT / "data"
+# DATA_DIR = PROJECT_ROOT / "data"
     
-    sample_path = os.path.join(DATA_DIR, "samples.csv")
+    sample_path = "data/samples.csv"
+    
     X = pd.read_csv(sample_path)
     y = pd.Series([0, 1, 0, 1])
 
